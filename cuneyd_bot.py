3e7ab81ac51b60c909288d65f70d2d067c306f01
Mert Bozfakioglu <mbozfakioglu@merts-mbp.lan>
2017-09-11 22:05:22 -0400

    cuneyd_bots

---
+++ cuneyd_bot.py
@@ -0,0 +1,16 @@
+class CuneydBot(Object):
+	def __init__(self,x,y,th,id,p = 1,sensors_xyt = [(-1,1,math.PI),(1,1,0),(0,1,math.Pi/2)]):
+		self.x = x
+		self.y = y
+		self.th = th
+		self.ID = ID
+		self.p = p
+		self.
+
+	def drive(self,v,w,t):
+		self.
+
+	def noisefy(self,percent_error_per_sec):
+		#noisefies input by percent_error_per_sec
+
+	def sensor(self,)
